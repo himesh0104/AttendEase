003138344b21d3e77c9d2037d5eea63d422f2bf0
Himesh <himeshkolcharlwar10@gmail.com>
2025-02-27 09:15:42 +0530

    aa

--- AttendEase/src/pages/FacultyDashboard.jsx
+++ AttendEase/src/pages/FacultyDashboard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Dashboard from "../components/Dashboard";
+import Dashboard from "../pages/Dashboard";
 
 const FacultyDashboard = () => {
   return <Dashboard userRole="FACULTY" />;
